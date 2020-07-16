e818cf72b15720b75c42ede55df84fdbce5fd716
rbabayoff <rbabayoff@gmail.com>
2020-07-17 01:57:49 +0300

    wip - changes due to formulas block changes

--- frontend/BlockFooter.tsx
+++ frontend/BlockFooter.tsx
@@ -1,5 +1,4 @@
-import log from "loglevel";
-log.setLevel("debug");
+import loglevel from "loglevel";
 
 import React from "react";
 
@@ -9,55 +8,62 @@ import { Layout, Typography } from "antd";
 const { Footer } = Layout;
 const { Text } = Typography;
 
-import {
-	StyledLink,
-	StyledLogoWrapper,
-	StyledLogoText,
-} from "./StyledComponents";
+import { StyledLogoWrapper, StyledLogoText } from "./StyledComponents";
+
+const log = loglevel.getLogger("BlockFooter");
+log.setLevel("debug");
 
 const StyledLogo = styled.img`
-	max-height: 12px;
+  max-height: 12px;
 `;
 
 const StyledFooterLogoText = styled(StyledLogoText)`
-	margin-left: 4px;
+  margin-left: 4px;
 `;
 
-export default function BlockFooter() {
-	return (
-		<Footer
-			style={{
-				position: "fixed",
-				overflow: "hidden",
-				bottom: 0,
-				zIndex: 1,
-				height: "24px",
-				width: "100%",
-				paddingTop: 0,
-				paddingBottom: 0,
-				backgroundColor: "white",
-				boxShadow: "rgba(0,0,0,0.1) 0 -2px 0 0",
-				display: "flex",
-				justifyContent: "flex-end",
-				alignItems: "center",
-				paddingRight: "8px",
-			}}
-		>
-			<Text
-				style={{
-					verticalAlign: "middle",
-					display: "inline-block",
-					marginRight: "4px",
-				}}
-			>
-				Powered by:{" "}
-			</Text>
-			<StyledLogoWrapper href="https://superblocks.at" target="_blank">
-				<StyledLogo src="https://superblocks.at/superblocks-logo-180x180/" />
-				<StyledFooterLogoText strong={true}>
-					Superblocks.at
-				</StyledFooterLogoText>
-			</StyledLogoWrapper>
-		</Footer>
-	);
+export default function BlockFooter({
+  position = "fixed",
+  bottom = 0
+}: {
+  position?: "fixed" | "absolute" | "relative" | "initial";
+  bottom?: number | "initial";
+}) {
+  log.debug("Footer.render");
+
+  return (
+    <Footer
+      style={{
+        position: position,
+        overflow: "hidden",
+        bottom: bottom,
+        zIndex: 200,
+        height: "24px",
+        width: "100%",
+        backgroundColor: "white",
+        boxShadow: "rgba(0,0,0,0.1) 0 -2px 0 0",
+        display: "flex",
+        justifyContent: "flex-end",
+        alignItems: "center",
+        paddingTop: 0,
+        paddingBottom: 0,
+        paddingRight: "8px"
+      }}
+    >
+      <Text
+        style={{
+          verticalAlign: "middle",
+          display: "inline-block",
+          marginRight: "4px"
+        }}
+      >
+        Powered by:{" "}
+      </Text>
+      <StyledLogoWrapper href="https://superblocks.at" target="_blank">
+        <StyledLogo src="https://superblocks.at/superblocks-logo-180x180/" />
+        <StyledFooterLogoText strong={true}>
+          Superblocks.at
+        </StyledFooterLogoText>
+      </StyledLogoWrapper>
+    </Footer>
+  );
 }
